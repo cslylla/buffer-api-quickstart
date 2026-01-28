d94b579e583e081b4931a60705f54545e7cda633
Lilla Csanaky <81328857+cslylla@users.noreply.github.com>
2026-01-28 12:31:48 +0100

    Add env example

--- examples/_common.py
+++ examples/_common.py
@@ -1,6 +1,9 @@
 import os
 import sys
 import requests
+from dotenv import load_dotenv
+
+load_dotenv()
 
 BASE_URL = os.getenv("BUFFER_BASE_URL", "http://127.0.0.1:8000")
 TOKEN = os.getenv("BUFFER_TOKEN")
